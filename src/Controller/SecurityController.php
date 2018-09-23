fad7d76d4b5473a0a9014064a829b11e546d0044
asteriXone <rubenrueda80@gmail.com>
2018-09-24 00:27:27 +0200

    Bootstrap Form Theme

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: asterixone
+ * Date: 24/09/2018
+ * Time: 0:06
+ */
+
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class SecurityController extends AbstractController
+{
+    /**
+     * @Route("/login", name="login")
+     */
+    public function login(AuthenticationUtils $authenticationUtils)
+    {
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', array(
+            'last_username' => $lastUsername,
+            'error'         => $error,
+        ));
+    }
+}
